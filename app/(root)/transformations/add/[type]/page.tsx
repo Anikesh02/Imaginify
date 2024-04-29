2a3617408eea07be8b72eb0d8afe7715d3c7dab5
Anikesh02 <anikeshkulalinft-@atharvacoe.ac.in>
2024-04-29 22:23:35 +0530

    Added Image Form for all features

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,10 +1,18 @@
 import Header from '@/components/shared/Header'
 import TransformationForm from '@/components/shared/TransformationForm'
 import { transformationTypes } from '@/constants'
+import { auth } from '@clerk/nextjs/server'
+import { getUserById } from '@/lib/actions/user.actions';
+import { redirect } from 'next/navigation';
 
-const AddTransformationTypePage = ({params:{type}}:SearchParamProps) => {
+const AddTransformationTypePage = async ({params:{type}}:SearchParamProps) => {
 
+  const {userId} = auth()
   const transformation= transformationTypes[type]
+
+  if(!userId) redirect('/sign-in')
+
+  const user = await getUserById(userId)
 
   return (
     <>
@@ -13,7 +21,15 @@ const AddTransformationTypePage = ({params:{type}}:SearchParamProps) => {
     subtitle={transformation.subTitle} 
     />
 
-    <TransformationForm />
+    <section className="mt-10">
+    <TransformationForm
+    action="Add"
+    userId={user._id}
+    type = {transformation.type as TransformationTypeKey}
+    creditBalance={user.creditBalance}
+    />
+    </section>
+
     </>
   )
 }
