a232bfc2ec0c044cb496af2c33d8c3cdeff682e7
Anikesh02 <anikeshkulalinft-@atharvacoe.ac.in>
2024-05-01 13:04:33 +0530

    Fix for Deployment and Middleware Errors

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,15 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
- 
-export default clerkMiddleware();
- 
+
+import {
+  clerkMiddleware,
+  createRouteMatcher
+} from '@clerk/nextjs/server';
+
+const isProtectedRoute = createRouteMatcher(['/', '/api/webhooks/clerk', '/api/webhooks/stripe']);
+
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth().protect();
+});
+
 export const config = {
-  matcher: [
-    '/((?!.*\\..*|_next).*)', // Don't run middleware on static files
-    '/', // Run middleware on index page
-    '/(api|trpc)(.*)'], // Run middleware on API routes
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
